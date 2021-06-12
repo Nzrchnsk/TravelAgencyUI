a88e06729c91380d3bba5307fbb1b62419ad5a89
Nikita Petin <nikita.pietin@gmail.com>
2021-06-12 18:44:13 +0300

    auth

--- src/main.js
+++ src/main.js
@@ -12,5 +12,13 @@ import 'bootstrap/dist/js/bootstrap.js'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
+axios.interceptors.request.use(function (config) {
+    config.headers.common = {
+        'accessToken': `Bearer ${localStorage.getItem('auth')}`,
+    };
+    return config;
+});
+
+
 
 createApp(App).use(store).use(VueAxios, axios).use(router).mount('#app')
