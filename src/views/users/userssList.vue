3aa4633ad78f32e4cde2f9712c37be4206853daf
Petin Nikita <petin.n@routeamgroup.com>
2021-06-15 14:34:39 +0300

    recommendation

--- src/views/users/userssList.vue
+++ src/views/users/userssList.vue
@@ -17,6 +17,10 @@
                     <td>{{item.email}}</td>
                     <td>{{item.role}}</td>
                     <td>
+                        <button type="button" @click="recommendedTrip(item.id)" data-bs-toggle="modal" data-bs-target="#recommendModal" class="btn btn-warning  me-2 btn-sm">
+                            Рекомендовать поездку
+                        </button>
+
                         <button type="button" @click="deleteUser(item.id)" class="btn btn-danger btn-sm">
                             Удалить
                         </button>
@@ -25,6 +29,29 @@
                 </tbody>
             </table>
         </div>
+
+        <!-- Modal -->
+        <div class="modal fade" id="recommendModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Рекомендация поездки</h5>
+                    </div>
+                    <div class="modal-body">
+                        <select v-model="modal.trip" required class="form-select mb-2" aria-label="Default select example">
+                            <option value="" selected disabled>Выберите маршрут</option>
+                            <option v-for="value in trips" :value="value.id">{{value.name}}</option>
+                        </select>
+                        <input type="text" v-model="modal.text" required class="form-control mb-2" placeholder="Введите рекомендацию">
+                        <div v-if="invalidValidation" style="color: red" class="mb-2"><label><small>Пожалуйста заполните все поля</small></label></div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" @click="recommend" class="btn btn-primary">Рекомендовать</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
     </div>
 </template>
 <script>
@@ -33,7 +60,14 @@
         name: "Users",
         data() {
             return {
+                invalidValidation: false,
+                modal: {
+                    user: null,
+                    trip: null,
+                    text: ''
+                },
                 users: [],
+                trips: [],
             };
         },
         components: {
@@ -43,6 +77,24 @@
             this.getUsers();
         },
         methods: {
+            recommendedTrip(user_id) {
+                this.modal.user = user_id;
+                this.getTrips();
+            },
+            async recommend() {
+                this.invalidValidation = false;
+                if (!this.modal.user || !this.modal.trip || !this.modal.text) {
+                    this.invalidValidation = true;
+                    return
+                }
+                try {
+                    await Api.Post('recommendation', this.modal);
+                    alert('Рекомендация успешно отправлена');
+                    $('#recommendModal').modal('hide')
+                } catch (e) {
+                    console.log(e)
+                }
+            },
             async deleteUser(id) {
                 try {
                     this.users = await Api.Delete('users/' + id);
@@ -55,6 +107,14 @@
                 try {
                     let {data} = await Api.Get('users');
                     this.users = data
+                } catch (e) {
+                    console.log(e)
+                }
+            },
+            async getTrips() {
+                try {
+                    let {data} = await Api.Get('trips');
+                    this.trips = data
                 } catch (e) {
                     console.log(e)
                 }
